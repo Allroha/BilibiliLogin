94f9bc7d05aec164837320551f289e4012b5dd93
afuet <73286581+lllk140@users.noreply.github.com>
2022-11-25 19:27:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from application.apps.app import sms
+from application.apps.app import sms_login, passport_login
+
 
 if __name__ == '__main__':
-    access_key, cookie = sms(86, 2233, ("7.6.0", "7060200"), ("SM-G955N", "9"))
+    kwargs = {
+        "versions": ("7.6.0", "7060200"),
+        "system": ("SM-G955N", "9")
+    }
+
+    access_key, cookie = sms_login("区号", "手机号", **kwargs)
+    # access_key, cookie = passport_login("用户名", "密码", **kwargs)
+
     print("access_key:", access_key)
     print("cookie:", cookie)
